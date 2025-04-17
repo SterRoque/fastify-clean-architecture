aac6cb99f6808a4dc4d55fa86b4a938d54944c7b
Sterphane Roque <stermiss2@gmail.com>
2025-04-17 01:31:35 -0300

    chore: change server port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-app.listen({ port: 3001 }, (err, address) => {
+app.listen({ port: 3333 }, (err, address) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
