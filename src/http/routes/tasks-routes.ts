24aaf66819b07dbc82d6e59d10bf1c04d21679dd
Sterphane Roque <stermiss2@gmail.com>
2025-04-18 18:27:30 -0300

    feat: add update and delete task routes

--- src/http/routes/tasks-routes.ts
+++ src/http/routes/tasks-routes.ts
@@ -1,9 +1,15 @@
 import { FastifyInstance } from "fastify";
 import { createTaskController } from "../controllers/create-task-controller";
 import { listTasksController } from "../controllers/list-tasks-controller";
+import { updateTaskController } from "../controllers/update-task-controller";
+import { deleteTaskController } from "../controllers/delete-task-controller";
 
 export async function tasksRoutes(app: FastifyInstance) {
   app.post("/tasks", createTaskController.execute);
 
   app.get("/tasks", listTasksController.execute);
+
+  app.patch("/tasks/:id", updateTaskController.execute);
+
+  app.delete("/tasks/:id", deleteTaskController.execute);
 }
